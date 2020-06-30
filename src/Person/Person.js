22948a7d24355757f2dbfae66296a609ee1d207e
Josh Schoenecker <schoeneckerj@gmail.com>
2020-06-30 17:07:24 -0600

    using props.children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
 
 // This is a functional comp, "stateless"
 
 // es6 syntax of making a function (arrow function)
 const person = (props) => {
-    return <p> Hello, my name is {props.name} and I am {props.age} years old!</p>
+  return (
+    <div>
+      <p>
+        Hello, my name is {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
 };
 
-export default person
+export default person;
