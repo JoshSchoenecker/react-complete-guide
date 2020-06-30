90758414beb7e5796ad9644967f188e66dea77ca
Josh Schoenecker <schoeneckerj@gmail.com>
2020-06-30 16:49:40 -0600

    functional comp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <Person />
+      </div>
+    );
+  }
   //  JSX compiles to the below
- // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Does this work?'))
+  // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Does this work?'))
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// This is a functional comp, "stateless"
+
+// es6 syntax of making a function (arrow function)
+const person = () => {
+    return <p> I'm a Person! </p>
+};
+
+export default person
